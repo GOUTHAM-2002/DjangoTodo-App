abf5e5d1f058047a02bb9ce6e2e06db409f55c10
goutham <goutham3336@gmail.com>
2024-04-18 06:34:56 +0530

    frontend enhancement

--- todos/forms.py
+++ todos/forms.py
@@ -1,14 +1,17 @@
 from django import forms
-from .models import Categories,Todos
+from .models import Todos,Categories
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todos
-        fields = ['title','category']
+        fields = ['category', 'title']  # Reordered fields
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Enter your task'}),
-            'category':forms.Select()
+
+            'title': forms.TextInput(attrs={'placeholder': 'Enter your task', 'style': 'margin-bottom:20px;width: 50%; border: 1px solid #ccc; padding: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'}),
+            'category': forms.Select(attrs={ 'style': 'margin-bottom:20px;width: 50%; border: 1px solid #ccc; padding: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);', 'placeholder':'Select a category'})
         }
+
+
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Categories
